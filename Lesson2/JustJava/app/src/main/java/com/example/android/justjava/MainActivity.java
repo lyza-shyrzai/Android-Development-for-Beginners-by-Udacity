7868533275da5462289f3f78b51bfb974c8aa14d
LizaSh <lizash605@gmail.com>
2017-01-11 18:38:27 +0200

    Added method for display price a coffee

--- Lesson2/JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ Lesson2/JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -12,6 +12,8 @@ import android.widget.TextView;
 
 import com.example.android.justjava.R;
 
+import java.text.NumberFormat;
+
 /**
  * This app displays an order form to order coffee.
  */
@@ -27,7 +29,8 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(77 * 2 + 1);
+        display(2);
+        displayPrice(2 * 5);
     }
 
     /**
@@ -36,6 +39,14 @@ public class MainActivity extends AppCompatActivity {
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
+    }
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }
 
