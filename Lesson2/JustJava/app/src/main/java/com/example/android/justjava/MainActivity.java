740ff5611af2ac5b11e7413ce8a92ef73c294ee2
LizaSh <lizash605@gmail.com>
2017-01-11 17:11:41 +0200

    Added Order coffee

--- Lesson2/JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ Lesson2/JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,8 +1,20 @@
+/**
+ * Add your package below. Package name can be found in the project's AndroidManifest.xml file.
+ * This is the package name our example uses:
+ */
+
 package com.example.android.justjava;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
+import com.example.android.justjava.R;
+
+/**
+ * This app displays an order form to order coffee.
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -10,4 +22,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        display(77 * 2 + 1);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
 }
+
