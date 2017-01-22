5022f05a7d8cb84fa582924d8253b99b40718e3b
LizaSh <lizash605@gmail.com>
2017-01-22 23:28:43 +0200

    Created Methods

--- PracticeSet2/CourtCounter/app/src/main/java/com/example/androidl/courtcounter/MainActivity.java
+++ PracticeSet2/CourtCounter/app/src/main/java/com/example/androidl/courtcounter/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.androidl.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Increase the score for Team A by 1 point.
+     */
+    public void addOneForTeamA(View v) {
+        displayForTeamA(1);
+    }
+
+    /**
+     * Increase the score for Team A by 2 point.
+     */
+    public void addTwoForTeamA(View v) {
+        displayForTeamA(2);
+    }
+
+    /**
+     * Increase the score for Team A by 3 point.
+     */
+    public void addThreeForTeamA(View v) {
+        displayForTeamA(3);
+    }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
     }
 }
