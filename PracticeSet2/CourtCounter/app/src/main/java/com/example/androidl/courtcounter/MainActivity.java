801f539d36ca01b89844093ba548e66deb5815c6
LizaSh <lizash605@gmail.com>
2017-01-23 21:46:14 +0200

    Created a Score Variable

--- PracticeSet2/CourtCounter/app/src/main/java/com/example/androidl/courtcounter/MainActivity.java
+++ PracticeSet2/CourtCounter/app/src/main/java/com/example/androidl/courtcounter/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    int scoreTeamA = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
