cda9c4a21010918ef772767fb70fa0d57d406913
LizaSh <lizash605@gmail.com>
2017-01-31 22:46:42 +0200

    Changed `displayMessage` Method

--- Lesson2/JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ Lesson2/JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -75,8 +75,8 @@ public class MainActivity extends AppCompatActivity {
      * This method displays the given text on the screen.
      */
     private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(message);
     }
 
     /**
