568e2dc885f49b1e5ef50c6d1de3aa04123259d5
LizaSh <lizash605@gmail.com>
2017-01-31 22:07:22 +0200

    Return Value from Method

--- Lesson2/JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ Lesson2/JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -32,21 +32,24 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * This method is called when the order button is clicked.
+     *
+     *
      */
     public void submitOrder(View view) {
-        String priceMessage = "$" + (quantity * 5) + " for " + quantity + " cups of coffee, please."
+        int price = calculatePrice();
+        String priceMessage = "$" + (price) + " for " + quantity + " cups of coffee, please."
                 + "\n" + "Thank you! :)";
         displayMessage(priceMessage);
-        calculatePrice(quantity);
     }
 
     /**
      * Calculates the price of the order.
      *
-     * @param quantity is the number of cups of coffee ordered
+     * @return total price
      */
-    private void calculatePrice(int quantity) {
+    private int calculatePrice() {
         int price = quantity * 5;
+        return price;
     }
 
     /**
