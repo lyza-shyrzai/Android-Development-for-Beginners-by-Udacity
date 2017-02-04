d467fcdac5a79c454672a4b0eadce9d0eea99f19
LizaSh <lizash605@gmail.com>
2017-02-04 22:06:14 +0200

    Added function which change text and image

--- Lesson3/Cookies/app/src/main/java/com/example/androidl/cookies/MainActivity.java
+++ Lesson3/Cookies/app/src/main/java/com/example/androidl/cookies/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.androidl.cookies;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.androidl.cookies.R;
 
@@ -20,8 +22,11 @@ public class MainActivity extends AppCompatActivity {
      */
     public void eatCookie(View view) {
         // TODO: Find a reference to the ImageView in the layout. Change the image.
+        ImageView beforeCookie = (ImageView) findViewById(R.id.android_cookie_image_view);
+        beforeCookie.setImageResource(R.drawable.after_cookie);
 
         // TODO: Find a reference to the TextView in the layout. Change the text.
-
+        TextView status = (TextView) findViewById(R.id.status_text_view);
+        status.setText("I am sooo full...");
     }
 }
