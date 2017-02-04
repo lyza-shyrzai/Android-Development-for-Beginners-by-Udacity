2b877c2202cf4c8329d41c7294333b6174202ea4
LizaSh <lizash605@gmail.com>
2017-02-04 21:49:44 +0200

    Added Java code

--- Lesson3/Cookies/app/src/main/java/com/example/androidl/cookies/MainActivity.java
+++ Lesson3/Cookies/app/src/main/java/com/example/androidl/cookies/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.androidl.cookies;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.example.androidl.cookies.R;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Called when the cookie should be eaten.
+     */
+    public void eatCookie(View view) {
+        // TODO: Find a reference to the ImageView in the layout. Change the image.
+
+        // TODO: Find a reference to the TextView in the layout. Change the text.
+
     }
 }
