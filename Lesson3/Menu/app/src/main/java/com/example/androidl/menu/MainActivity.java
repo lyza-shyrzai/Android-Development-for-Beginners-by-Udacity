e5aa17542905cd82796517b07ca42f24c7f6dd86
LizaSh <lizash605@gmail.com>
2017-02-05 00:01:48 +0200

    Added code for printing logs

--- Lesson3/Menu/app/src/main/java/com/example/androidl/menu/MainActivity.java
+++ Lesson3/Menu/app/src/main/java/com/example/androidl/menu/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.androidl.menu;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -15,10 +17,18 @@ public class MainActivity extends AppCompatActivity {
 
     public void printToLogs(View view) {
         // Find first menu item TextView and print the text to the logs
+        TextView Item1 = (TextView) findViewById(R.id.menu_item_1);
+        String menuItem1 = Item1.getText().toString();
+        Log.v("MainActivity", menuItem1);
 
         // Find second menu item TextView and print the text to the logs
+        TextView Item2 = (TextView) findViewById(R.id.menu_item_2);
+        String menuItem2 = Item2.getText().toString();
+        Log.v("MainActivity", menuItem2);
 
         // Find third menu item TextView and print the text to the logs
-
+        TextView Item3 = (TextView) findViewById(R.id.menu_item_3);
+        String menuItem3 = Item3.getText().toString();
+        Log.v("MainActivity", menuItem3);
     }
 }
